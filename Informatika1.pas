2519433aafd1a0e4e0da04a93452e1cf5174e4f4
marizhe <60063329+marizhe@users.noreply.github.com>
2020-01-19 14:43:40 +0300

    Добавлен файл informatika1

---
+++ Informatika1.pas
@@ -0,0 +1,49 @@
+﻿program Strashnoyezadaniyepoinformatike;
+
+begin
+
+// №A
+writeln ('Димa');
+writeln ('   пошел');
+writeln ('       погулять');
+
+writeln ('');
+writeln ('----------------------------');
+writeln ('');
+
+
+// №B
+writeln ('   ж ');
+writeln ('  жжж ');
+writeln (' жжжжж ');
+writeln ('жжжжжжж');
+writeln (' нн нн');
+writeln (' zzzzz');
+writeln ('');
+writeln ('----------------------------');
+writeln ('');
+
+// №C(1)
+writeln ('+++++++++++++');
+writeln (' +++ +++ +++');
+writeln ('  +   +  +');
+writeln ('  +   +  +');
+writeln (' +++ +++ +++');
+writeln ('+++++++++++++');
+writeln ('');
+writeln ('----------------------------');
+writeln ('');
+
+// №C(1)
+writeln ('+         +');
+writeln ('+ +     + +');
+writeln ('+ + + + + +');
+writeln ('+ +     + +');
+writeln ('+         +');
+writeln ('+         +');
+writeln ('+ +     + +');
+writeln ('+ + + + + +');
+writeln ('+ +     + +');
+writeln ('+         +');
+
+end.
